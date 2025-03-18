2de810e0d9e188553821d5fcddf7dacd355bc788
Marius Jabami <138234634+mariusjabami@users.noreply.github.com>
2025-03-18 22:46:19 +0100

    Atualizar o index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Foundation by Marius Jabami</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
